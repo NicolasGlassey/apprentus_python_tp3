82ba0c202162c375bd990c2bc78a4466ae036382
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-07-30 18:41:18 +0200

    Empty Model

--- fraction.py
+++ fraction.py
@@ -3,20 +3,14 @@ class FractionError(Exception):
 
 
 class Fraction:
-    numerator = 0;
-    denominator = 0;
-
     def __init__(self, numerator=1, denominator=1):
-        if denominator == 0:
-            raise FractionError
-        self.numerator = numerator
-        self.denominator = denominator
+        pass
 
     def value(self):
-        return self.numerator / self.denominator
+        pass
 
     def get_numerator(self):
-        return self.numerator
+        pass
 
     def get_denominator(self):
-        return self.denominator
+        pass
