f57a4da2c39826664d4f984218c57e1d744b17f6
Marie-France <mfdeboccard@gmail.com>
2022-08-01 14:15:35 +0200

    Update test class

---
+++ test/TestFractionII.py
@@ -0,0 +1,58 @@
+import unittest
+
+from fraction import FractionError
+from fraction import Fraction
+
+
+class TestFraction(unittest.TestCase):
+    def test_getters_numerator_nominal_case_success(self):
+        """given"""
+        expected_numerator = 10
+        expected_denominator = 2
+        fraction = Fraction(expected_numerator, expected_denominator)
+
+        """when"""
+
+        """then"""
+        self.assertEqual(expected_numerator, fraction.get_numerator())
+        self.assertEqual(expected_denominator, fraction.get_denominator())
+
+    def test_getters_numerator_default_value_success(self):
+        """given"""
+        expected_numerator = 1
+        expected_denominator = 1
+        fraction = Fraction()
+
+        """when"""
+
+        """then"""
+        self.assertEqual(expected_numerator, fraction.get_numerator())
+        self.assertEqual(expected_denominator, fraction.get_denominator())
+
+    def test_constructor_zero_value_denominator_throwException(self):
+            """given"""
+            expected_numerator = 1
+            expected_denominator = 0
+            fraction = Fraction()
+
+            """when"""
+
+            """then"""
+            self.assertRaises(FractionError, fraction.value())
+
+    def test_value_nominal_case_throwException(self):
+            """given"""
+            numerator = 10
+            denominator = 5
+            expected_result = 2
+            fraction = Fraction(numerator, denominator)
+
+            """when"""
+            actual_result = fraction.value()
+
+            """then"""
+            self.assertEqual(expected_result, actual_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
